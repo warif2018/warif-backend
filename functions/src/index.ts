187a19265ed0732dba948b9b31dda3e2c58f7ae2
tomohidemiya <t.miya19890131@gmail.com>
2019-05-06 15:12:23 +0900

    init
    初期化

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.get('/:id', (req, res) => res.send(req.params.id));
+app.post('/', (req, res) => res.send('It Works!'));
+
+exports.warif = functions.https.onRequest(app);
