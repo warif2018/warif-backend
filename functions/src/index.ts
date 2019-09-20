0958cae2b95aa5a7ff36a2c985e64656f3045073
tomohidemiya <t.miya19890131@gmail.com>
2019-09-21 02:00:43 +0900

    Revert "init"
    This reverts commit 187a19265ed0732dba948b9b31dda3e2c58f7ae2.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,8 @@
 import * as functions from 'firebase-functions';
-import * as express from 'express';
-import * as cors from 'cors';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-const app = express();
-// Automatically allow cross-origin requests
-app.use(cors({ origin: true }));
-
-app.get('/:id', (req, res) => res.send(req.params.id));
-app.post('/', (req, res) => res.send('It Works!'));
-
-exports.warif = functions.https.onRequest(app);
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
